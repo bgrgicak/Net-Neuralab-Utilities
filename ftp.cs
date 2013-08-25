f646762327f9dc220e938de100abc0f7bb691de2
bgrgicak <bgrgicak@foi.hr>
2013-08-25 14:36:55 +0200

    ftp.cs localData.cs
    ftp.cs omogučava pohranu i preuzimanje podataka s ftp poslužitelja.
    localData.cs omogučava preimenovanje lokalnog podatka i određivanje
    putanje do projekta

---
+++ ftp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Net_Neuralab_Utilities
+{
+    /// <summary>
+    /// kalsa za preuzimenje i pohranu podataka na FTP server
+    /// </summary>
+    class ftp
+    {
+        private string host;
+        /// <summary>
+        /// host je potpuna adresa hosta (user@host), ako želimo pohraniti u neki određeni folder na serveru dodajemo putanju u naziv hosta (user@host/folder)
+        /// </summary>
+        private string user;
+        private string pass;
+        /// <summary>
+        /// user je korisničko ime, a pass šifra korisnika na poslužitelju
+        /// </summary>
+        
+        public ftp(string host,string uName,string pass) {
+            this.host = host;
+            this.user = uName;
+            this.pass = pass;
+        }
+
+        /// <summary>
+        /// metoda za dohvačanje podataka s ftp servera, potrebno je zadati naziv podatka na serveru, drugi argument je putanja za pohranu na lokalnom računalu 
+        /// </summary>
+        /// <param name="src"></param>
+        public void getFrom(string src,string loc)
+        {
+            //postavljanje veze
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://"+host+"/"+ src);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials = new NetworkCredential(user, pass);
+            //preuzimanje podataka
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            //obrada i pohrana
+            Stream responseStream = response.GetResponseStream();
+            System.IO.FileInfo file = new System.IO.FileInfo(loc);
+            file.Directory.Create();
+            using (Stream s = File.Create(loc+"/"+src))
+            {
+                responseStream.CopyTo(s);
+            }
+            response.Close();
+        }
+        /// <summary>
+        /// preuzima podatak definiran prvim arhumentom s lokalnog računala zadanu drugim argumentom, i pohranjuje na server
+        /// </summary>
+        /// <param name="name"></param>
+        public void setTo(string name,string path)
+        {
+
+            // postavljanje veze
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + host + "/" + name);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(user, pass);
+
+            // priprema podatka
+            StreamReader sourceStream = new StreamReader(path+"/"+name);
+            byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            request.ContentLength = fileContents.Length;
+            //slanje
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+            requestStream.Close();
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            response.Close();
+        }
+    }
+}
---
+++ localData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Net_Neuralab_Utilities
+{
+    /// <summary>
+    /// metode za rad s lokalnim podacima
+    /// </summary>
+    /// <param name="src"></param>
+    class localData
+    {
+        /// <summary>
+        /// projectDir vrača trenutnu lokaciju projekta na računalu
+        /// </summary>
+        /// <returns></returns>
+        public string projectDir() {
+            return Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        }
+        /// <summary>
+        /// renameFile preimenuje podatak koji se nalazi na source lokaciji u naziv zadan drugim parametrom
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public void renameFile(string source, string name) {
+            var path = Path.GetDirectoryName(source);
+            File.Move(source, path + name);
+        }
+    }
+}
